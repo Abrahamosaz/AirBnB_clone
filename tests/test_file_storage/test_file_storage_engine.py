e7c6fa008b87b71ef11641186fd2afeab50266a9
abraham <abrahamosazee3@gmail.com>
2022-10-29 14:10:42 +0100

    Updated tests files

--- tests/test_file_storage/test_file_storage_engine.py
+++ tests/test_file_storage/test_file_storage_engine.py
@@ -4,10 +4,12 @@ import json
 import models
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+import pep8
 import unittest
 """
 Test for the file storage engine database
 """
+
 
 class TestFileStorage(unittest.TestCase):
     """
@@ -16,13 +18,21 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         self.user_model = BaseModel()
 
+    def Pep8StyleGuide(self):
+        """
+        Test pep8 style guide
+        """
+        pep8_style = pep8.StyleGuide(quite=True)
+        result = pep8_style.check_files(["models/engine/file_storage.py"])
+        self.seertEqual(result, 0, "Found errors in pe8 style guide (Warning)")
+
     def test_module_doc(self):
         self.assertTrue(len(FileStorage.__doc__) > 1)
 
     def test_attribute(self):
         self.assertIsInstance(models.storage._FileStorage__objects, dict)
         self.assertIsInstance(models.storage._FileStorage__file_path, str)
-    
+
     def test_new_method(self):
         user1_model = BaseModel()
         user2_model = BaseModel()
@@ -31,7 +41,9 @@ class TestFileStorage(unittest.TestCase):
         models.storage.new(user3_model)
         for key, value in models.storage._FileStorage__objects.items():
             self.assertTrue(isinstance(value, BaseModel))
-            self.assertEqual(key, "BaseModel" + "." + key.split(sep=".")[1])
+            key_values = key.split(sep=".")
+            self.assertEqual(key,
+                             "{}.{}".format(key_values[0], key_values[1]))
 
     def test_all_method(self):
         objects = models.storage.all()
@@ -43,9 +55,10 @@ class TestFileStorage(unittest.TestCase):
 
     def test_save_method(self):
         models.storage.save()
-        with open(models.storage._FileStorage__file_path, mode="r", encoding="utf-8") as file:
+        with open(models.storage._FileStorage__file_path,
+                  mode="r", encoding="utf-8") as file:
             json_string = json.load(file)
         new_dict = {}
         for key, value in models.storage._FileStorage__objects.items():
             new_dict[key] = value.to_dict()
-        self.assertTrue(new_dict  == json_string)
+        self.assertTrue(new_dict == json_string)
